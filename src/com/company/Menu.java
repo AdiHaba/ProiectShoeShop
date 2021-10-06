57b8f9301d60f4c92909f041ad2f3d19a36737fa
AdiHaba <91590014+AdiHaba@users.noreply.github.com>
2021-10-06 13:15:29 +0300

    Update Menu.java

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 
 public class Menu {
     private Scanner sc;
-
-    public Menu(){
+    private Object Shoes;
+    int option;
+    public Menu() {
         System.out.println("Menu:");
     }
-    int option;
-    public void PrincipalMenu(){
+
+    public void PrincipalMenu() {
         System.out.println("Welcome");
         System.out.println("What do you want?");
         System.out.println("1.Products 2.Your account 3.Budget 4.Exit site");
         option = sc.nextInt();
 
-        switch (option)
-        {
+        switch (option) {
             case 1:
                 new Shoes();
                 break;
@@ -28,29 +28,48 @@ public class Menu {
                 System.exit(0);
                 return;
             default:
-                System.out.println("Unvalible");
+                System.out.println("Unavalible");
                 break;
         }
 
     }
 
-    public void Shoes() {
+    public void BuyShoes() {
         System.out.println("Menu");
         System.out.println("1. Our shoes");
         System.out.println("2. Check budget");
         System.out.println("3. Buy shoes");
         System.out.println("4. Exit program");
-        switch(option){
+        switch (option) {
             case 1:
-                System.out.println("This are our shoes");{
-                    public void ShowShoes(){
-                        for (int i = 1; i<6; ++i){
-                            System.out.println("/n" + i + Shoes[i]);
-                        }
+                System.out.println("This are our shoes");
+                CheckStock();
 
-                }
+            case 2:
+                System.out.println("Your budget is: " + Budget._sold);
+                BuyShoes();
+                return;
+            case 3:
+                Buy();
+                BuyShoes();
+            case 4:
+                System.exit(0);
+                break;
+            default:
+                break;
         }
+
+    }
+
+    private void CheckStock() {
+    }
+
+    private void Buy() {
+        System.out.println("Chose your shoes by the number");
+        int option = sc.nextInt();
     }
 
 
-}
+    }
+
+
