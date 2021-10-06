9527c1cb0f8ff643094d532d0e331869cd0126d1
AdiHaba <91590014+AdiHaba@users.noreply.github.com>
2021-10-06 13:15:44 +0300

    Update Shoes.java

--- src/com/company/Shoes.java
+++ src/com/company/Shoes.java
@@ -7,13 +7,13 @@ public class Shoes {
     private double size;
     private double price;
     int n;
-   Shoes[] stock;
+    Shoes[] stock;
 
     public Shoes() {
         this.brand = brand;
     }
 
-    public Shoes(String brand, String model, String color,double size, double price) {
+    public Shoes(String brand, String model, String color, double size, double price) {
         this.brand = brand;
         this.model = model;
         this.color = color;
@@ -26,15 +26,23 @@ public class Shoes {
     }
 
 
-    public void stock(){
+    public void Stock() {
         stock = new Shoes[n];
 
-        stock[1] = new Shoes("Adidas","SuperStar", "White", 39, 300);
+        stock[1] = new Shoes("Adidas", "SuperStar", "White", 39, 300);
         stock[2] = new Shoes("Adidas", "SuperStar", "Black", 42, 290);
-        stock[3] = new Shoes("Nike", "AirForce", "Grey", 37, 250 );
+        stock[3] = new Shoes("Nike", "AirForce", "Grey", 37, 250);
         stock[4] = new Shoes("Nike", "Mercurial", "Green-Orange", 43, 350);
-        stock[5]= new Shoes("Vans", "Sk8-High", "Black-White", 40, 301);
+        stock[5] = new Shoes("Vans", "Sk8-High", "Black-White", 40, 301);
 
 
+    }
+
+    public void CheckStock() {
+        int n = 6;
+        for (int i = 1; i < n; ++i) {
+            System.out.println("/n" + i + stock[i]);
+            return;
+        }
     }
 }
