ad2b3d2882a5d5992d34003407a2bee977fe8e81
AdiHaba <91590014+AdiHaba@users.noreply.github.com>
2021-10-06 13:15:04 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Buyer buyer = new Buyer();
         Budget budget = new Budget(0);
         Shoes shoes = new Shoes();
         Menu menu = new Menu();
-
     }
 }
+
